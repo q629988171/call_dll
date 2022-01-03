200b5c4dd24546649549061dfb58f29af3e1018e
q629988171 <113960096@qq.com>
2022-01-03 16:40:05 +0800

    Add files via upload

---
+++ C++/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	//声明函数指针，告诉程序从动态链接库导入的地址是什么类型的函数
+	typedef int(*test)(char *errmsg);
+
+	// 加载DLL
+	HINSTANCE hDll = LoadLibrary(R"(C:\Users\Allen\source\repos\Dll4\Debug\Dll4.dll)");
+
+	//查找函数地址
+	test p_test = (test)GetProcAddress(hDll, "test");
+
+	char tmp[MAX_PATH];
+	
+	// 使用dll中导出的函数
+	p_test(tmp);
+
+	//打印结果
+	cout << tmp << endl;
+
+	//释放DLL
+	FreeLibrary(hDll);
+
+	return 0;
+}
